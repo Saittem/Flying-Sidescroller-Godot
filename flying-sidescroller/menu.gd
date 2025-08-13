28b73ae75e2c2cd669e8e0fb77d0a475c742c26f
Saittem <TEOLF@email.cz>
2025-08-13 20:46:30 +0200

    Added menu
    Features:
    Menu contains buttons Start (only level 1), Options (unused), Quit

---
+++ flying-sidescroller/menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+var level_1 = "res://levels/level_1.tscn"
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file(level_1)
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
