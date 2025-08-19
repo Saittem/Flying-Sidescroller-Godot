7e07b23e337d255729ed1bf262c7195e8c80944c
Saittem <TEOLF@email.cz>
2025-08-19 22:20:42 +0200

    Update player
    Features:
    When player collides with power up, he gain force shield and can take one hit without taking damage. Force shield is then removed

--- flying-sidescroller/scripts/player.gd
+++ flying-sidescroller/scripts/player.gd
@@ -6,6 +6,8 @@ signal health_changed
 
 @export var player_movement_speed: float = 500
 @onready var player_animation_sprite = $PlayerAnimation
+@onready var player_shield_animation = $ForceShieldAnimation
+@onready var player_particle_animation = $ParticleAnimation
 
 const PLAYER_MAX_HEALTH : int = 100
 var player_current_health : int = PLAYER_MAX_HEALTH
@@ -23,7 +25,8 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 	elif area.is_in_group("health_up"):
 		change_health(25)
 	elif area.is_in_group("power_up"):
-		pass
+		activate_shield()
+		area.queue_free()
 
 func get_input():
 	var character_direction_y: float = Input.get_axis("move_up", "move_down")
@@ -32,6 +35,17 @@ func get_input():
 		velocity.y = character_direction_y * player_movement_speed
 	else:
 		velocity.y = velocity.move_toward(Vector2.ZERO, player_movement_speed).y
+
+func activate_shield():
+	player_shield_animation.visible = true
+
+func deactivate_shield():
+	player_particle_animation.visible = true
+	player_particle_animation.play("default")
+
+func _on_particle_animation_animation_finished() -> void:
+	player_particle_animation.visible = false
+	player_shield_animation.visible = false
 
 func update_animation():
 	if velocity.y > 0:
@@ -42,10 +56,13 @@ func update_animation():
 		player_animation_sprite.play("idle")
 
 func change_health(health_difference: int):
-	player_current_health += health_difference
-	if player_current_health > 100:
-		player_current_health = 100
+	if !player_shield_animation.visible or health_difference > 0:
+		player_current_health += health_difference
+		if player_current_health > 100:
+			player_current_health = 100
+		else:
+			health_changed.emit()
+			if player_current_health <= 0:
+				pass
 	else:
-		health_changed.emit()
-		if player_current_health <= 0:
-			pass
+		deactivate_shield()
