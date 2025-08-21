2d21dbd6c637264633dd3bb789ec947638f09c77
Saittem <TEOLF@email.cz>
2025-08-21 23:56:41 +0200

    Update menu.gd

--- flying-sidescroller/menu.gd
+++ flying-sidescroller/menu.gd
@@ -2,10 +2,10 @@ extends Control
 
 @onready var level_1 = "res://levels/level_1.tscn"
 @onready var level_2 = "res://levels/level_2.tscn"
-#var level_1 = "res://levels/level_1.tscn"
+@onready var level_3 = "res://levels/level_3.tscn"
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file(level_2)
+	get_tree().change_scene_to_file(level_3)
 
 func _on_options_pressed() -> void:
 	pass # Replace with function body.
