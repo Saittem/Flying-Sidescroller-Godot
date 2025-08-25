a5a87a591018e17cafc9a6c83f198edc2fe5f323
Saittem <TEOLF@email.cz>
2025-08-25 23:31:12 +0200

    Update menu
    Features:
    Buttons play audio on hover and pressed

--- flying-sidescroller/menu.gd
+++ flying-sidescroller/menu.gd
@@ -8,17 +8,40 @@ extends Control
 @onready var level_layer = $"Levels"
 @onready var option_layer = $"Options"
 
-# Main Menu buttons
-func _on_start_pressed() -> void:
-	menu_layer.visible = false
-	level_layer.visible = true
+#Audio
+@onready var hover_audio = $HoverAudio
+@onready var click_audio = $ClickAudio
 
-func _on_options_pressed() -> void:
-	menu_layer.visible = false
-	option_layer.visible = true
+#Buttons
+@onready var start_button = $"MainMenu/VBoxContainer/Start"
+@onready var options_button = $"MainMenu/VBoxContainer/Options"
+@onready var quit_button = $"MainMenu/VBoxContainer/Quit"
 
-func _on_quit_pressed() -> void:
-	get_tree().quit()
+func _ready() -> void:
+	start_button.connect("pressed", Callable(self, "_button_pressed").bind(start_button))
+	options_button.connect("pressed", Callable(self, "_button_pressed").bind(options_button))
+	quit_button.connect("pressed", Callable(self, "_button_pressed").bind(quit_button))
+	
+	start_button.mouse_entered.connect(_button_mouse_entered)
+	options_button.mouse_entered.connect(_button_mouse_entered)
+	quit_button.mouse_entered.connect(_button_mouse_entered)
+
+#Buttons
+func _button_pressed(button):
+	click_audio.play()
+	
+	match button.name:
+		"Start":
+			menu_layer.visible = false
+			level_layer.visible = true
+		"Options":
+			menu_layer.visible = false
+			option_layer.visible = true
+		"Quit":
+			get_tree().quit()
+
+func _button_mouse_entered():
+	hover_audio.play()
 
 # Level buttons
 func _on_level_1_button_pressed() -> void:
