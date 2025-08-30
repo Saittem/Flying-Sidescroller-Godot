e70440f93784b893a6de5db3baa03ca24b910b47
Saittem <TEOLF@email.cz>
2025-08-30 23:39:31 +0200

    Added audio to all buttons

--- flying-sidescroller/menu.gd
+++ flying-sidescroller/menu.gd
@@ -17,10 +17,15 @@ extends Control
 @onready var options_button = $"MainMenu/VBoxContainer/Options"
 @onready var quit_button = $"MainMenu/VBoxContainer/Quit"
 
+@onready var level_1_button = $Levels/Level1/Level1Button
+@onready var level_2_button = $Levels/Level2/Level2Button
+@onready var level_3_button = $Levels/Level3/Level3Button
+
 #Best progress labels
 @onready var best_progress_level_1_label = $"Levels/Level1/BestProgress"
 @onready var best_progress_level_2_label = $"Levels/Level2/BestProgress"
 @onready var best_progress_level_3_label = $"Levels/Level3/BestProgress"
+
 
 func _ready() -> void:
 	get_tree().paused = false
@@ -28,10 +33,16 @@ func _ready() -> void:
 	start_button.connect("pressed", Callable(self, "_button_pressed").bind(start_button))
 	options_button.connect("pressed", Callable(self, "_button_pressed").bind(options_button))
 	quit_button.connect("pressed", Callable(self, "_button_pressed").bind(quit_button))
+	level_1_button.connect("pressed", Callable(self, "_button_pressed").bind(level_1_button))
+	level_2_button.connect("pressed", Callable(self, "_button_pressed").bind(level_2_button))
+	level_3_button.connect("pressed", Callable(self, "_button_pressed").bind(level_3_button))
 	
 	start_button.mouse_entered.connect(_button_mouse_entered)
 	options_button.mouse_entered.connect(_button_mouse_entered)
 	quit_button.mouse_entered.connect(_button_mouse_entered)
+	level_1_button.mouse_entered.connect(_button_mouse_entered)
+	level_2_button.mouse_entered.connect(_button_mouse_entered)
+	level_3_button.mouse_entered.connect(_button_mouse_entered)
 	
 	best_progress_level_1_label.text = "Best: " + str(Global.best_progress_level_1) + "%"
 	best_progress_level_2_label.text = "Best: " + str(Global.best_progress_level_2) + "%"
@@ -56,10 +67,13 @@ func _button_mouse_entered():
 
 # Level buttons
 func _on_level_1_button_pressed() -> void:
+	await get_tree().create_timer(0.1).timeout
 	get_tree().change_scene_to_file(level_1)
 
 func _on_level_2_button_pressed() -> void:
+	await get_tree().create_timer(0.1).timeout
 	get_tree().change_scene_to_file(level_2)
 
 func _on_level_3_button_pressed() -> void:
+	await get_tree().create_timer(0.1).timeout
 	get_tree().change_scene_to_file(level_3)
