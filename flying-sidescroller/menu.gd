533459106159f8e56aee52d9c0376dc536630324
Saittem <TEOLF@email.cz>
2025-08-31 00:27:46 +0200

    Added option menu and back button

--- flying-sidescroller/menu.gd
+++ flying-sidescroller/menu.gd
@@ -26,6 +26,15 @@ extends Control
 @onready var best_progress_level_2_label = $"Levels/Level2/BestProgress"
 @onready var best_progress_level_3_label = $"Levels/Level3/BestProgress"
 
+#Toggle
+@onready var sound_toggle = $Options/GridContainer/SoundsToggle
+@onready var save_toggle = $Options/GridContainer/SaveToggle
+
+#Toggle labels
+@onready var sound_state_label = $Options/GridContainer/SoundState
+@onready var save_state_label = $Options/GridContainer/SaveState
+
+@onready var back_button = $BackButton
 
 func _ready() -> void:
 	get_tree().paused = false
@@ -44,6 +53,9 @@ func _ready() -> void:
 	level_2_button.mouse_entered.connect(_button_mouse_entered)
 	level_3_button.mouse_entered.connect(_button_mouse_entered)
 	
+	sound_toggle.connect("pressed", Callable(self, "_toggle_pressed").bind(sound_toggle))
+	save_toggle.connect("pressed", Callable(self, "_toggle_pressed").bind(save_toggle))
+	
 	best_progress_level_1_label.text = "Best: " + str(Global.best_progress_level_1) + "%"
 	best_progress_level_2_label.text = "Best: " + str(Global.best_progress_level_2) + "%"
 	best_progress_level_3_label.text = "Best: " + str(Global.best_progress_level_3) + "%"
@@ -51,6 +63,7 @@ func _ready() -> void:
 #Buttons
 func _button_pressed(button):
 	click_audio.play()
+	back_button.visible = true
 	
 	match button.name:
 		"Start":
@@ -65,6 +78,26 @@ func _button_pressed(button):
 func _button_mouse_entered():
 	hover_audio.play()
 
+#Toggle
+func _toggle_pressed(toggle_button):
+	click_audio.play()
+	
+	match toggle_button.name:
+		"SoundsToggle":
+			Global.sound_toggle = !Global.sound_toggle
+			
+			if Global.sound_toggle:
+				sound_state_label.text = "On"
+			else:
+				sound_state_label.text = "Off"
+		"SaveToggle":
+			Global.save_toggle = !Global.save_toggle
+			
+			if Global.save_toggle:
+				save_state_label.text = "On"
+			else:
+				save_state_label.text = "Off"
+
 # Level buttons
 func _on_level_1_button_pressed() -> void:
 	await get_tree().create_timer(0.1).timeout
@@ -77,3 +110,9 @@ func _on_level_2_button_pressed() -> void:
 func _on_level_3_button_pressed() -> void:
 	await get_tree().create_timer(0.1).timeout
 	get_tree().change_scene_to_file(level_3)
+
+func _on_back_button_pressed() -> void:
+	level_layer.visible = false
+	option_layer.visible = false
+	menu_layer.visible = true
+	back_button.visible = false
