e66dc51b369114db7edad703c6f4355e8f8e2660
Saittem <TEOLF@email.cz>
2025-09-01 22:47:08 +0200

    Updated death screen GUI

--- flying-sidescroller/death_screen.gd
+++ flying-sidescroller/death_screen.gd
@@ -2,8 +2,23 @@ extends Control
 
 var menu_scene = "res://menu.tscn"
 
+@onready var restart_button = $HBoxContainer/RestartButton
+@onready var menu_button = $HBoxContainer/MenuButton
+
+@onready var hover_audio = $HoverAudio
+@onready var click_audio = $ClickAudio
+
+func _ready() -> void:
+	restart_button.mouse_entered.connect(_button_mouse_entered)
+	menu_button.mouse_entered.connect(_button_mouse_entered)
+
 func _on_restart_button_pressed() -> void:
 	get_tree().reload_current_scene()
+	click_audio.play()
 
 func _on_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file(menu_scene)
+	click_audio.play()
+
+func _button_mouse_entered():
+	hover_audio.play()
