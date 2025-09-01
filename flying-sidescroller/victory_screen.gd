d024c354f426a5124dee6820fb09523da2761057
Saittem <TEOLF@email.cz>
2025-09-01 22:46:59 +0200

    Updated victory screen GUI

--- flying-sidescroller/victory_screen.gd
+++ flying-sidescroller/victory_screen.gd
@@ -2,5 +2,12 @@ extends Control
 
 var menu_scene = "res://menu.tscn"
 
+@onready var hover_audio = $HoverAudio
+@onready var click_audio = $ClickAudio
+
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file(menu_scene)
+	click_audio.play()
+
+func _on_button_mouse_entered() -> void:
+	hover_audio.play()
